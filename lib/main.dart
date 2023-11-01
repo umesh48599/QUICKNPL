e7fb36a4ec911aa9527124f5e6a1cb066e083985
umesh48599 <krishnaghimire25@gmail.com>
2023-11-01 13:34:59 +0545

    i have add some pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:quicknpl/login.dart';
+import 'package:quicknpl/signin_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: 'login',
-    routes: {'login':(context)=> MyLogin()},
-  
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  //this widget is the root of your application
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        //This is the theme of your application
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignInScreen(),
+    );
+  }
 }
---
+++ lib/signin_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.black, Colors.white])),
+      ),
+    );
+  }
+}
